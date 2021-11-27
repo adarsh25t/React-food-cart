3a6e95424abb3b50e0b2d04726c928f6da644552
adarsh25t <adarsh25t@gmail.com>
2021-11-27 12:49:48 +0530

    header add cart button HeaderCartButton

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,12 +1,14 @@
 import { Fragment } from "react";
 import HeaderImg from "../../assets/meals.jpg";
 import classes from './Header.module.css';
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = ()=>{
     return(
         <Fragment>
             <header className={classes.header}>
-                <h1>FoodHub</h1>
+                <h3>FoodHub</h3>
+                <HeaderCartButton/>
             </header>
             <div className={classes['main-image']}>
                 <img src={HeaderImg} alt="" />
